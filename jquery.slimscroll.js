dfa4de35b71f3c2ac23388caff89ca5d8f2e3a61
Nikunj Savaliya <savaliyanikunj2@gmail.com>
2025-02-03 19:13:33 +0530

    t is not defined error solved

--- jquery.slimscroll.js
+++ jquery.slimscroll.js
@@ -226,7 +226,7 @@ function init($) {
           bar.bind("mousedown", function(e) {
             var $doc = $(document);
             isDragg = true;
-            t = parseFloat(bar.css('top'));
+            var t = parseFloat(bar.css('top'));
             pageY = e.pageY;
 
             $doc.bind("mousemove.slimscroll", function(e){
